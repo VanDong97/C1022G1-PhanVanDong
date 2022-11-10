fa115206792e63f85aad35a25b781b6d905ce3fd
vandong97 <phanvandong19977@gmail.com>
2022-11-10 16:27:56 +0700

    fix comment

--- module1/Btap/ss9/chi_so_can_nang.js
+++ module1/Btap/ss9/chi_so_can_nang.js
@@ -1,14 +1,14 @@
-function kiem_tra(){
-    let can_nang = document.getElementById("can_nang").value;
-    let chieu_cao = document.getElementById("chieu_cao").value;
-    let bmi = can_nang / (chieu_cao * chieu_cao);
+function kiem_tra() {
+    let canNang = document.getElementById("canNang").value;
+    let chieuCao = document.getElementById("chieuCao").value;
+    let bmi = canNang / (chieuCao * chieuCao);
     let result = "";
 
     if (bmi < 18.5)
         result = "Quá gầy. Nên ăn nhiều vào!"
     else if (bmi < 25)
-        result = "Hơi gầy. Nên xem lại chế độ ăn"    
-    else if(bim < 30)
+        result = "Hơi gầy. Nên xem lại chế độ ăn"
+    else if (bim < 30)
         result = "Cơ thể cân đối"
     else result = "Có hiện tượng béo phì"
     document.getElementById("result").innerHTML = "Chỉ số BMI :" + bmi + "Bạn" + result;
